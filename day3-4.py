e1184d25ca372f7462a9df8d83e6491e351b83fe
mickeyli0515 <68846074+mickeyli0515@users.noreply.github.com>
2020-07-29 16:32:43 +0800

    Add files via upload

---
+++ day3-4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 11:45:33 2020
+
+@author: user
+"""
+
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+x,y,z = mc.player.getTilePos()
+for i in range(20):
+    mc.setBlock(x+2,y-1,z+i,57)
+    z = z+2
+
+
+
